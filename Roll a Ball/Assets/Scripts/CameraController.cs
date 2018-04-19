2f0ccf536772ae9acd3d5c0155d1c9c0b1f4b9a4
Aaron Trafford <uenvymenow@gmail.com>
2018-04-19 04:43:59 -0400

    Working on Roll a Ball Tutorial. Left off at the collection of the objects by player

---
+++ Roll a Ball/Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	// Creates a GameObject variable to access the player and a Vector3 variable to hold the offset position
+	public GameObject player;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+	
+	// LateUpdate runs every frame, but after all items have been processed in update
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+}
---
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// Creates a float for handling movement speed
+	public float speed;
+
+	// Creates a variable to hold for accessing the RigidBody componenet
+	private Rigidbody rb;
+
+	void Start()
+	{
+		// Accesses RigidBody component
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void Update() // Update called before rendering frame. Most game code goes here
+	{
+		
+	}
+
+	void FixedUpdate() // Called just before performing any physics calculations. Where physics code goes
+	{
+		// Gets current positions once movement keys are pressed
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		// Creates a new movement vector to use in add force
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		// Adds force when movement keys are pressed according to the movement Vector created
+		rb.AddForce (movement * speed);
+	}
+
+}
